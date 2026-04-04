abe99267c5737f1fa8630e2e9c00fab0fb9e4004
GG <gerardstjosol772@gmail.com>
2026-04-04 22:31:18 +0800

    Enemy Addition
    Added Map chunk improvement and enemy movement

---
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    Transform player;
+    public float moveSpeed;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, player.position, moveSpeed * Time.deltaTime);
+    }
+}
--- Assets/Scripts/Map/MapController.cs
+++ Assets/Scripts/Map/MapController.cs
@@ -1,107 +1,171 @@
 using NUnit.Framework;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class MapController : MonoBehaviour
 {
-    public List<GameObject> terrainChunks; // List of terrain chunk prefabs to choose from
-    public GameObject player; // Reference to the player GameObject
-    public float checkerRadius; // Size of each terrain chunk
-    Vector3 noTerrainPosition;
-    public LayerMask terrainMask;
-    PlayerMovement playerMovement;
-    public GameObject currentChunk;
+    public Camera referenceCamera;
+    public float checkInterval = 0.5f;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("Chunk Settings")]
+    public PropRandomizer[] terrainChunks;
+    public Vector2 chunkSize = new Vector2(20f, 20f);
+    public LayerMask terrainMask = 1;
+    public bool deleteCulledChunks = false;
+
+    // Stores the last camera's position and size.
+    // To determine whether we need to do checks.
+    Vector3 lastCameraPosition;
+    Rect lastCameraRect;
+    float cullDistanceSqr;
+
     void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player"); // Find the player GameObject by its tag
-        playerMovement = player.GetComponent<PlayerMovement>(); // Get the PlayerMovement component from the player GameObject
+        // Print out errors when important variables are not assigned.
+        if (!referenceCamera)
+            Debug.LogError("MapController cannot work without a reference camera.");
+
+        if (terrainChunks.Length < 1)
+            Debug.LogError("There are no Terrain Chunks assigned, so the map cannot be dynamically generated.");
+
+        // Begin the map checking coroutine.
+        StartCoroutine(HandleMapCheck());
+        HandleChunkSpawning(Vector2.zero, true);
     }
 
-    // Update is called once per frame
-    void Update()
+    void Reset()
     {
-        ChunkChecker(); // Call the method to check for terrain chunks and spawn new ones if necessary
+        referenceCamera = Camera.main;
     }
 
-    void ChunkChecker()
+    // Coroutine that runs periodically to check and spawn new map pieces.
+    IEnumerator HandleMapCheck()
     {
-        if (!currentChunk)
+        for (; ; )
         {
-            return; // If there is no current chunk, exit the method
-        }
+            yield return new WaitForSeconds(checkInterval);
 
-        if (playerMovement.moveInput.x > 0 && playerMovement.moveInput.y == 0) //right
-        {
-            if (!Physics2D.OverlapCircle(currentChunk.transform.Find("Right").position, checkerRadius, terrainMask))
+            // Only update the map if one of these is true.
+            Vector3 moveDelta = referenceCamera.transform.position - lastCameraPosition;
+            bool hasCamWidthChanged = !Mathf.Approximately(referenceCamera.pixelWidth - lastCameraRect.width, 0),
+                 hasCamHeightChanged = !Mathf.Approximately(referenceCamera.pixelHeight - lastCameraRect.height, 0);
+
+            if (hasCamWidthChanged || hasCamHeightChanged || moveDelta.magnitude > 0.1f)
             {
-                noTerrainPosition = currentChunk.transform.Find("Right").position;
-                SpawnChunk();
+                HandleChunkCulling();
+                HandleChunkSpawning(moveDelta, true);
             }
-        }
-        else if (playerMovement.moveInput.x == 0 && playerMovement.moveInput.y > 0) //up
-        {
-            if (!Physics2D.OverlapCircle(currentChunk.transform.Find("Up").position, checkerRadius, terrainMask))
-            {
-                noTerrainPosition = currentChunk.transform.Find("Up").position;
-                SpawnChunk();
-            }
-        }
-        else if (playerMovement.moveInput.x < 0 && playerMovement.moveInput.y == 0) //left
-        {
-            if (!Physics2D.OverlapCircle(currentChunk.transform.Find("Left").position, checkerRadius, terrainMask))
-            {
-                noTerrainPosition = currentChunk.transform.Find("Left").position;
-                SpawnChunk();
-            }
-        }
-        else if (playerMovement.moveInput.x == 0 && playerMovement.moveInput.y < 0) //down
-        {
-            if (!Physics2D.OverlapCircle(currentChunk.transform.Find("Down").position, checkerRadius, terrainMask))
-            {
-                noTerrainPosition = currentChunk.transform.Find("Down").position;
-                SpawnChunk();
-            }
-        }
-        else if (playerMovement.moveInput.x > 0 && playerMovement.moveInput.y > 0) //right up
-        {
-            if (!Physics2D.OverlapCircle(currentChunk.transform.Find("Up Right").position, checkerRadius, terrainMask))
-            {
-                noTerrainPosition = currentChunk.transform.Find("Up Right").position;
-                SpawnChunk();
-            }
-        }
-        else if (playerMovement.moveInput.x > 0 && playerMovement.moveInput.y < 0) //right down
-        {
-            if (!Physics2D.OverlapCircle(currentChunk.transform.Find("Down Right").position, checkerRadius, terrainMask))
-            {
-                noTerrainPosition = currentChunk.transform.Find("Down Right").position;
-                SpawnChunk();
-            }
-        }
-        else if (playerMovement.moveInput.x < 0 && playerMovement.moveInput.y > 0) //left up
-        {
-            if (!Physics2D.OverlapCircle(currentChunk.transform.Find("Up Left").position, checkerRadius, terrainMask))
-            {
-                noTerrainPosition = currentChunk.transform.Find("Up Left").position;
-                SpawnChunk();
-            }
-        }
-        else if (playerMovement.moveInput.x < 0 && playerMovement.moveInput.y < 0) //left down
-        {
-            if (!Physics2D.OverlapCircle(currentChunk.transform.Find("Down Left").position, checkerRadius, terrainMask))
-            {
-                noTerrainPosition = currentChunk.transform.Find("Down Left").position;
-                SpawnChunk();
-            }
+
+            lastCameraPosition = referenceCamera.transform.position;
+            lastCameraRect = referenceCamera.pixelRect;
         }
     }
 
-    void SpawnChunk()
+    // Gets a rect that represents the area the camera covers in the game world.
+    public Rect GetWorldRectFromViewport()
     {
-        int randomIndex = Random.Range(0, terrainChunks.Count); // Get a random index for the terrain chunk prefabs
-        Instantiate(terrainChunks[randomIndex], noTerrainPosition, Quaternion.identity); // Spawn the terrain chunk at the noTerrainPosition
+        if (!referenceCamera)
+        {
+            Debug.LogError("Reference camera not found. Using Main Camera instead.");
+            referenceCamera = Camera.main;
+        }
+
+        Vector2 minPoint = referenceCamera.ViewportToWorldPoint(referenceCamera.rect.min),
+                maxPoint = referenceCamera.ViewportToWorldPoint(referenceCamera.rect.max);
+        Vector2 size = new Vector2(maxPoint.x - minPoint.x, maxPoint.y - minPoint.y);
+        cullDistanceSqr = Mathf.Max(size.sqrMagnitude, chunkSize.sqrMagnitude) * 3;
+
+        return new Rect(minPoint, size);
+    }
+
+    // Gets all the points we have to check for chunks on.
+    public Vector2[] GetCheckedPoints()
+    {
+        Rect viewArea = GetWorldRectFromViewport();
+        Vector2Int tileCount = new Vector2Int(
+            (int)Mathf.Ceil(viewArea.width / chunkSize.x) + 1,
+            (int)Mathf.Ceil(viewArea.height / chunkSize.y) + 1
+        );
+
+        HashSet<Vector2> result = new HashSet<Vector2>();
+        for (int y = -1; y < tileCount.y; y++)
+        {
+            for (int x = -1; x < tileCount.x; x++)
+            {
+                result.Add(new Vector2(
+                    viewArea.min.x + chunkSize.x * x,
+                    viewArea.min.y + chunkSize.y * y
+                ));
+            }
+        }
+
+        return result.ToArray();
+    }
+
+    void HandleChunkSpawning(Vector2 moveDelta, bool checkWithoutDelta = false)
+    {
+
+        HashSet<Vector2> spawnedPositions = new HashSet<Vector2>();
+        Vector2 currentPosition = referenceCamera.transform.position;
+
+        // Checks all the viewport points we are interested in.
+        foreach (Vector3 vp in GetCheckedPoints())
+        {
+            if (!checkWithoutDelta)
+            {
+                // Only check left / right if we are moving.
+                if (moveDelta.x > 0 && vp.x < 0.5f) continue;
+                else if (moveDelta.x < 0 && vp.x > 0.5f) continue;
+
+                // Only check up / down if we are moving.
+                if (moveDelta.y > 0 && vp.y < 0.5f) continue;
+                else if (moveDelta.y < 0 && vp.y > 0.5f) continue;
+            }
+
+            // Snaps the checked position to the nearest chunked position.
+            Vector3 checkedPosition = SnapPosition(vp);
+
+            // If the position has no chunks, then spawn chunk.
+            if (!spawnedPositions.Contains(checkedPosition) && !Physics2D.OverlapPoint(checkedPosition, terrainMask))
+                SpawnChunk(checkedPosition);
+
+            spawnedPositions.Add(checkedPosition);
+        }
+    }
+
+    // Rounds a Vector to the nearest position as given by chunkSize.
+    Vector3 SnapPosition(Vector3 position)
+    {
+        return new Vector3(
+            Mathf.Round(position.x / chunkSize.x) * chunkSize.x,
+            Mathf.Round(position.y / chunkSize.y) * chunkSize.y,
+            transform.position.z
+        );
+    }
+
+    // Spawns a chunk at a designated position.
+    PropRandomizer SpawnChunk(Vector3 spawnPosition, int variant = -1)
+    {
+        if (terrainChunks.Length < 1) return null;
+        int rand = variant < 0 ? Random.Range(0, terrainChunks.Length) : variant;
+        PropRandomizer chunk = Instantiate(terrainChunks[rand], transform);
+        chunk.transform.position = spawnPosition;
+        return chunk;
+    }
+
+    // Determines whether a given chunk should be shown or hidden.
+    void HandleChunkCulling()
+    {
+        for (int i = transform.childCount - 1; i >= 0; i--)
+        {
+            Transform chunk = transform.GetChild(i);
+            Vector2 dist = referenceCamera.transform.position - chunk.position;
+            bool cull = dist.sqrMagnitude > cullDistanceSqr;
+            chunk.gameObject.SetActive(!cull);
+            if (deleteCulledChunks && cull) Destroy(chunk.gameObject);
+        }
     }
 }
